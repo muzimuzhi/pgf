c99d0015757c87d7361e10bb911c299da37f5711
muzimuzhi <muzimuzhi@gmail.com>
2022-01-14 14:51:16 +0800

    test(gd): Load `pgf`'s support file
    Signed-off-by: muzimuzhi <muzimuzhi@gmail.com>

--- config-gd.lua
+++ config-gd.lua
@@ -1,5 +1,9 @@
 -- Tests for graph drawing (gd) library
 
-stdengine    = "luatex"
-checkengines = {"luatex"}
-testfiledir  = "testfiles-gd"
+stdengine      = "luatex"
+checkengines   = {"luatex"}
+testfiledir    = maindir .. "/testfiles-gd"
+
+-- extra support file(s)
+supportdir     = maindir .. "/testfiles/support"
+checksuppfiles = {"pgf-regression-test.tex"}
